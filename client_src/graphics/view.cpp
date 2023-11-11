fb8d486c7938a6aaf9ca05e60597f9da050ef1b8
Altovolta <cavila@fi.uba.ar>
2023-11-11 18:24:35 -0300

    fix: error de compilacion

--- client_src/graphics/view.cpp
+++ client_src/graphics/view.cpp
@@ -4,8 +4,7 @@
 GameView::GameView() : sdl(SDL_INIT_VIDEO | SDL_INIT_AUDIO), window("Worms", SDL_WINDOWPOS_UNDEFINED,
                                                    SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_RESIZABLE),
                        renderer(window, -1, SDL_RENDERER_ACCELERATED),
-                       mixer(MIX_DEFAULT_FREQUENCY, 0x8010, 2, 4096),
-                       music(DATA_PATH "/sonidos/worms_music.mp3")
+                       mixer(MIX_DEFAULT_FREQUENCY, 0x8010, 2, 4096)
 {
     tex_manager.loadTexture(this->renderer);//carga las texturas de los sprites
     tex_manager.loadBackground(this->renderer);//carga la texturas del fondo
