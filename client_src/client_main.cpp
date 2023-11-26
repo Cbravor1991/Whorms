260a9b15012653bb650a4387dd634564d0a2981e
Cbravor1991 <cbravor@fi.uba.ar>
2023-11-26 01:50:33 -0300

    subo linkeo appa y cuadro

--- client_src/client_main.cpp
+++ client_src/client_main.cpp
@@ -22,10 +22,10 @@ int main(int argc, char *argv[])
 
         /*PARA PROBAR LA APP DE QT DESCOMENTAR ESTAS LINEAS*/
         QApplication app(argc, argv);
-        // Lobby lobby(hostname, servname);
-        // MainWindow w(&lobby);
-        // w.show();
-        // app.exec();
+        Lobby lobby(hostname, servname);
+        MainWindow w(&lobby);
+         w.show();
+        app.exec();
 
         /*PARA PROBAR LA APP DE QT COMENTAR ESTAS DOS LINEAS*/
         Game juego(hostname, servname);
